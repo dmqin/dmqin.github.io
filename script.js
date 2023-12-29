5cbc172a26b3615cfe38d5a27226eb0cebdcdb8e
dmqin <dimqin2004@gmail.com>
2023-12-29 19:14:59 +0700

    menambahkan animasi dengan jquery

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("#profile").waypoint(function(){
+      $("#propic").addClass
+      ("animate__animated animate__fadeInLeft");
+      $("#profileParagraph").addClass
+      ("animate__animated animate__fadeInRight");
+    })
+   
+   });
+
+  
+   $(document).ready(function(){
+    $("#features").waypoint(function(){
+      $("#interestText").addClass
+      ("animate__animated animate__slideInLeft");
+      $("#interestPicture").addClass
+      ("animate__animated animate__zoomIn");
+    })
+   
+   });
